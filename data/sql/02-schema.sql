699f067a845047fc30fd6a7197ffc28285163d68
Florian Abélard <f.abelard@gmail.com>
2018-05-15 23:01:40 +0200

    feat(tournament): update default database

--- data/sql/02-schema.sql
+++ data/sql/02-schema.sql
@@ -5,3 +5,17 @@ CREATE TABLE player (
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY (uuid)
 );
+
+CREATE TABLE tournament (
+    uuid VARCHAR(36) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (uuid)
+);
+
+CREATE TABLE tournament_player (
+    tournament_uuid VARCHAR(36) NOT NULL,
+    player_uuid VARCHAR(36) NOT NULL,
+    PRIMARY KEY (tournament_uuid, player_uuid),
+    FOREIGN KEY (tournament_uuid) REFERENCES tournament (uuid) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (player_uuid) REFERENCES player (uuid) ON DELETE CASCADE ON UPDATE CASCADE
+)
