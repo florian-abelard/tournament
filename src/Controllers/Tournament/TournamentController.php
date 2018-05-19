411fb3f0b8871f4aef68b8062a13831109d37b6f
Florian Abélard <f.abelard@gmail.com>
2018-05-19 11:25:40 +0200

    feat(tournament): display list and show tournament

--- src/Controllers/Tournament/TournamentController.php
+++ src/Controllers/Tournament/TournamentController.php
@@ -4,7 +4,7 @@ namespace Flo\Tournoi\Controllers\Tournament;
 
 use Flo\Tournoi\Domain\Core\ValueObjects\Uuid;
 use Flo\Tournoi\Domain\Tournament\Entities\Tournament;
-use Flo\Tournoi\Domain\Player\PlayerRepository;
+use Flo\Tournoi\Domain\Tournament\TournamentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,15 +12,24 @@ use Symfony\Component\HttpFoundation\Response;
 class TournamentController extends Controller
 {
     private
-        $playerRepository;
+        $tournamentRepository;
 
-    public function __construct(PlayerRepository $repository)
+    public function __construct(TournamentRepository $repository)
     {
-        $this->playerRepository = $repository;
+        $this->tournamentRepository = $repository;
     }
 
     public function displayList(): Response
     {
-        return $this->render('Tournament/listTournament.html.twig');
+        $tournaments = $this->tournamentRepository->findAll();
+
+        return $this->render('Tournament/listTournament.html.twig', array('tournaments' => $tournaments));
+    }
+
+    public function show(string $uuid): Response
+    {
+        $tournament = $this->tournamentRepository->findById(new Uuid($uuid));
+
+        return $this->render('Tournament/showTournament.html.twig', array('tournament' => $tournament));
     }
 }
