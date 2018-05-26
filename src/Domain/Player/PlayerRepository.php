f676f4479b156a38a1ff5cfcb8784d2c759bc8ac
Florian Abélard <f.abelard@gmail.com>
2018-05-27 00:10:59 +0200

    fix(player): complete player repository interface

--- src/Domain/Player/PlayerRepository.php
+++ src/Domain/Player/PlayerRepository.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Flo\Tournoi\Domain\Player;
 
 use Flo\Tournoi\Domain\Core\ValueObjects\Uuid;
+use Flo\Tournoi\Domain\Player\Collections\PlayerCollection;
 use Flo\Tournoi\Domain\Player\Entities\Player;
 
 interface PlayerRepository
@@ -13,7 +14,13 @@ interface PlayerRepository
 
     public function findById(Uuid $uuid): ?Player;
 
-    public function findAll(): iterable;
+    public function findAll(): PlayerCollection;
+
+    public function findByTournamentId(Uuid $tournamentUuid): PlayerCollection;
+
+    public function findNotInTournament(Uuid $tournamentUuid): PlayerCollection;
 
     public function remove(Uuid $uuid): void;
+
+    private function buildDomainObject(array $result): Player;
 }
