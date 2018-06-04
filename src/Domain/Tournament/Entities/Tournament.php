f80d6e18717a22113408edb8c9b4d2e0281acd48
Florian Abélard <f.abelard@gmail.com>
2018-06-04 21:58:23 +0200

    fix(registration): remove Registration call in entities

--- src/Domain/Tournament/Entities/Tournament.php
+++ src/Domain/Tournament/Entities/Tournament.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace Flo\Tournoi\Domain\Tournament\Entities;
 
 use Flo\Tournoi\Domain\Core\ValueObjects\Uuid;
-use Flo\Tournoi\Domain\Registration\Collections\RegistrationCollection;
 use Flo\Tournoi\Domain\Registration\Entities\Registration;
 use Flo\Tournoi\Persistence\Tournament\DataTransferObjects as DTO;
 
@@ -13,14 +12,12 @@ class Tournament
 {
     private
         $uuid,
-        $name,
-        $registrations;
+        $name;
 
     public function __construct(Uuid $uuid, string $name)
     {
         $this->uuid = $uuid;
         $this->name = $name;
-        $this->registrations = new RegistrationCollection();
     }
 
     public function uuid(): Uuid
@@ -31,11 +28,6 @@ class Tournament
     public function name(): string
     {
         return $this->name;
-    }
-
-    public function addRegistration(Registration $registration): void
-    {
-        $this->registrations->add($registration);
     }
 
     public function toDTO(): DTO\Tournament
