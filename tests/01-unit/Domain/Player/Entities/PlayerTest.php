6335dc85593992d719d266a8d7da5efe68cedc02
Florian Abélard <f.abelard@gmail.com>
2018-06-04 21:59:09 +0200

    feat(tests): test entities

--- tests/01-unit/Domain/Player/Entities/PlayerTest.php
+++ tests/01-unit/Domain/Player/Entities/PlayerTest.php
@@ -6,6 +6,7 @@ namespace Flo\Tournoi\Tests\Domain\Player\Entities;
 
 use Flo\Tournoi\Domain\Core\ValueObjects\Uuid;
 use Flo\Tournoi\Domain\Player\Entities\Player;
+use Flo\Tournoi\Persistence\Player\DataTransferObjects as DTO;
 use PHPUnit\Framework\TestCase;
 
 class PlayerTest extends TestCase
@@ -22,11 +23,24 @@ class PlayerTest extends TestCase
         $this->points = 501;
     }
 
-    public function testTest()
+    public function testCreateEntity()
     {
         $player = new Player($this->uuid, $this->name);
         $player->setPoints($this->points);
 
+        $this->assertEquals($this->uuid, $player->uuid());
+        $this->assertEquals($this->name, $player->name());
         $this->assertEquals($this->points, $player->points());
+    }
+
+    public function testToDTO()
+    {
+        $player = new Player($this->uuid, $this->name);
+        $player->setPoints($this->points);
+
+        $dto = $player->toDTO();
+
+        $this->assertInstanceOf(DTO\Player::class, $dto);
+        $this->assertSame($player->uuid()->value(), $dto->uuid());
     }
 }
