28c30c38df4a1cdad8ff4a05bf7c5f1093409f6a
Florian Abélard <f.abelard@gmail.com>
2018-06-20 20:06:58 +0200

    feat(group): add Game entity [WIP]

---
+++ src/Domain/Game/Entities/Game.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Flo\Tournoi\Domain\Game\Entities;
+
+use Flo\Tournoi\Domain\Player\Entities\Player;
+
+class Game
+{
+    private
+        $isPlayed,
+        $winner,
+        $player1,
+        $player2,
+        $sets;
+
+    public function __construct(Player $player1, Player $player2)
+    {
+        $this->player1 = $player1;
+        $this->player2 = $player2;
+
+        // $this->sets = new SetCollection();
+    }
+}
