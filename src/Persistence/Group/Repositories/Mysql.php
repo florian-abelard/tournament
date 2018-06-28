af95721f211871986b5659cff26b02b842b00606
Florian Abélard <f.abelard@gmail.com>
2018-06-28 22:46:29 +0200

    feat(group): add findByStageId method in repository

--- src/Persistence/Group/Repositories/Mysql.php
+++ src/Persistence/Group/Repositories/Mysql.php
@@ -6,6 +6,7 @@ namespace Flo\Tournoi\Persistence\Group\Repositories;
 
 use Flo\Tournoi\Domain\Group\Collections\GroupCollection;
 use Flo\Tournoi\Domain\Player\Entities\Player;
+use Flo\Tournoi\Domain\Player\ValueObjects\RankingPoints;
 use Flo\Tournoi\Persistence\Core\Repositories\Mysql as MysqlCore;
 use Flo\Tournoi\Domain\Core\ValueObjects\Uuid;
 use Flo\Tournoi\Domain\Group\Entities\Group;
@@ -14,7 +15,9 @@ use Flo\Tournoi\Domain\Group\GroupRepository;
 class Mysql extends MysqlCore implements GroupRepository
 {
     private const
-        TABLE = 'group';
+        TABLE = 'group',
+        PLAYER_JOIN_TABLE = 'group_player',
+        PLAYER_TABLE = 'player';
 
     public function persist(Group $group): void
     {
@@ -41,7 +44,7 @@ SQL;
 
     public function addPlayer(Uuid $uuid, Player $player): void
     {
-        $table = 'group_player';
+        $table = self::PLAYER_JOIN_TABLE;
         $playerDto = $player->toDTO();
 
         $sql = <<<SQL
@@ -57,6 +60,71 @@ SQL;
 
     public function findByStageId(Uuid $stageUuid): GroupCollection
     {
-        return new GroupCollection();
+        $table = self::TABLE;
+        $joinTable = self::PLAYER_JOIN_TABLE;
+        $playerTable = self::PLAYER_TABLE;
+
+        $sql = <<<SQL
+            SELECT gr.*, gp.player_uuid, pl.name, pl.ranking_points
+            FROM `$table` as gr
+            INNER JOIN `$joinTable` as gp
+            ON gr.uuid = gp.group_uuid
+            INNER JOIN `$playerTable` as pl
+            ON gp.player_uuid = pl.uuid
+            WHERE stage_uuid = :stageUuid
+            ORDER BY pl.ranking_points DESC
+SQL;
+
+        $statement = $this->getDatabaseConnection()->prepare($sql);
+        $statement->bindValue(':stageUuid', $stageUuid->value());
+        $statement->execute();
+
+        $groups = new GroupCollection();
+        
+        $resultGroupedBy = [];
+        foreach ($statement->fetchAll(\PDO::FETCH_ASSOC) as $result)
+        {
+            $resultGroupedBy[$result['uuid']][] = $result;
+        }
+
+        foreach ($resultGroupedBy as $groupUuid => $resultByGroup)
+        {
+            $group = $this->buildDomainObject($result);
+
+            foreach ($resultByGroup as $result)
+            {
+                $group->addPlayer(
+                    $this->buildPlayerDomainObject($result)
+                );
+            }
+
+            $groups->add($group);
+        }
+
+        return $groups;
+    }
+
+    public function buildDomainObject(array $result): Group
+    {
+        $group = new Group(
+            new Uuid($result['uuid']),
+            new Uuid($result['stage_uuid'])
+        );
+        $group->setLabel($result['label']);
+        $group->setPlacesNumber((int) $result['places_number']);
+
+        return $group;
+    }
+
+    public function buildPlayerDomainObject(array $result): Player
+    {
+        $player = new Player(
+            new Uuid($result['player_uuid']),
+            $result['name']
+        );
+
+        $player->setRankingPoints(RankingPoints::fromString($result['ranking_points']));
+
+        return $player;
     }
 }
